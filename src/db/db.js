4ee5a3aa2a2c9ebdd8b8cfbdde20ff7d14624b1d
Satyam <sm2003satyam@gmail.com>
2025-07-18 22:51:28 +0530

    Fix database configuration: update database field to use DB_NAME instead of DB_USER

--- src/db/db.js
+++ src/db/db.js
@@ -8,7 +8,7 @@ const { Pool } = pg;
 const pool = new Pool({
     user: process.env.DB_USER,
     host: "localhost",
-    database: process.env.DB_USER,
+    database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
 });
