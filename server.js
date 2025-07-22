bf774de872769021a9e03feeff4ab0fbb90252c4
Satyam <sm2003satyam@gmail.com>
2025-07-22 21:12:17 +0530

    Remove database connection logic from server.js and simplify server startup

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 import { app } from "./app.js";
-import pool from "./src/db/db.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const Port = process.env.PORT || 3000;
 
-pool.connect()
-    .then(() => {
-        console.log("Connected to PostgreSql");
-        app.listen(Port, () => {
-            console.log(`Server is listening at port ${Port}`);
-        });
-    })
-    .catch((err) => console.error("Database connection failed", err));
+app.listen(Port, () => {
+    console.log(`Server is listening at port ${Port}`);
+});
