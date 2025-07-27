aed863bf65c513db338e2566f2edb7d053d05854
Satyam <sm2003satyam@gmail.com>
2025-07-27 22:17:41 +0530

    Add token generation functions for access and refresh tokens

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,4 +1,5 @@
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
 
 export const encryptPassword = async (password) => {
     const hashedPassword = await argon2.hash(password);
@@ -8,4 +9,28 @@ export const encryptPassword = async (password) => {
 export const validatePassword = async (password, hashedPassword) => {
     const result = await argon2.verify(hashedPassword, password);
     return result;
+}
+
+export const generateAccessToken = async (payload) => {
+    try {
+        return jwt.sign(
+            { payload },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+        );
+    } catch (error) {
+        throw new Error("Error while generating access token");
+    }
+}
+
+export const generateRefreshToken = async (payload) => {
+    try {
+        return jwt.sign(
+            { payload },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+        );
+    } catch (error) {
+        throw new Error("Error while generating refresh token");
+    }
 }
