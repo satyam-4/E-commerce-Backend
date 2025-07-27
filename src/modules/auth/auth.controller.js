8d9289c00ff7fc44bfaacd16622ceffbecad93ef
Satyam <sm2003satyam@gmail.com>
2025-07-27 22:18:10 +0530

    Add token generation and storage in signinUser function

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,5 +1,5 @@
-import { checkUserExistence, createNewUser, getUserByEmail } from "./auth.db.js";
-import { encryptPassword, validatePassword } from "./auth.service.js";
+import { checkUserExistence, createNewUser, getUserByEmail, storeRefreshToken } from "./auth.db.js";
+import { encryptPassword, generateAccessToken, generateRefreshToken, validatePassword } from "./auth.service.js";
 
 const signupUser = async (req, res) => {
     const { fullName, email, password, phone } = req.body;
@@ -26,20 +26,34 @@ const signupUser = async (req, res) => {
 
 const signinUser = async (req, res) => {
     const { email, password } = req.body;
-    const result = await getUserByEmail(email);
+    const user = await getUserByEmail(email);
 
-    if(!result) {
+    if(!user) {
         throw new Error("User does not exist");
     } 
 
-    const hashedPassword = result.password;
+    const hashedPassword = user.password;
     
     if(!(await validatePassword(password, hashedPassword))) {
         throw new Error("Incorrect password");
     }
 
+    const payload = { id: user.id, email: user.email, role: user.role };
+    const accessToken = await generateAccessToken(payload);
+    const refreshToken = await generateRefreshToken(payload);
+
+    await storeRefreshToken(user.id, refreshToken);
+
     return res 
     .status(200)
+    .cookie("accessToken", accessToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000
+    })
+    .cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000
+    })
     .json({
         success: true,
         message: "User logged in successfully"
