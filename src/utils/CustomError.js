b2b91fd09a952764429696d02dacc9ed6877ae95
Satyam <sm2003satyam@gmail.com>
2025-08-06 21:17:58 +0530

    Add custom error class

---
+++ src/utils/CustomError.js
@@ -0,0 +1,23 @@
+class CustomError extends Error {
+    constructor(
+        statusCode = 500, 
+        message = "Internal server error",
+        errors = [],
+        stack = ""
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors;
+        this.success = false;
+        this.data = null;
+
+        if(stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.stack);
+        }
+
+    }
+};
+
+export { CustomError };
