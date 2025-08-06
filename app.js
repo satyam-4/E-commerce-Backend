14db8b91e6ccc7bbd66303f23a0cbe4c3694f9bb
Satyam <sm2003satyam@gmail.com>
2025-08-06 21:20:40 +0530

    use custom error handler middleware

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
+import authRoutes from "./src/modules/auth/auth.route.js";
+import { errorHandler } from "./src/middlewares/errorHandler.middleware.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hi-Hello");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/auth", authRoutes);
 
+app.use(errorHandler);
 export { app };
