3c75f9b64335e64bb30a8b669130ce32eddc5636
Satyam <sm2003satyam@gmail.com>
2025-08-16 21:35:02 +0530

    Add route for user profile retrieval with JWT verification

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { signinUser, signupUser } from "./auth.controller.js";
+import { getMe, signinUser, signupUser } from "./auth.controller.js";
+import { verifyJWT } from "#middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.route("/signin").post(signinUser);
 router.route("/signup").post(signupUser);
+router.route("/me").get(verifyJWT, getMe);
 
 export default router;
