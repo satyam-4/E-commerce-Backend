0404ef536faf3f2b00812c4ee4965764a11f9958
Satyam <sm2003satyam@gmail.com>
2025-08-16 21:33:07 +0530

    Add cookie parser middleware to the application

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import authRoutes from "./src/modules/auth/auth.route.js";
 import { errorHandler } from "./src/middlewares/errorHandler.middleware.js";
+import cookieParser from "cookie-parser";;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use("/auth", authRoutes);
 
 app.use(errorHandler);
