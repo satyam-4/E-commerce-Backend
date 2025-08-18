848eb0a84781cb67fc9bda7fcf97d3668b5e5f77
Satyam <sm2003satyam@gmail.com>
2025-08-18 20:26:21 +0530

    Add product controller with CRUD operations for products

---
+++ src/modules/product/product.controller.js
@@ -0,0 +1,88 @@
+import { AppError } from "#utils/AppError.js";
+import { addProducts, getAllProducts, getProductById, updateProductById } from "./product.db.js";
+
+const createProducts = async (req, res) => {
+    const userId = req.user.id;
+    const { name, price, description } = req.body;
+    
+    const product = await addProducts(userId, name, price, description);
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        message: "Product added sucessfully",
+        product: product
+    });
+};
+
+const getProducts = async (req, res) => {
+    const products = await getAllProducts();
+    return res
+    .status(200)
+    .json({
+        success: true,
+        message: "Products fetched successfully",
+        data: products
+    });
+};
+
+const getProductsById = async (req, res) => {
+    const { id } = req.params;
+    const productId = parseInt(id, 10);
+    const product = await getProductById(productId);
+
+    if(!product) {
+        throw new AppError(404, "Product not found");
+    }
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        message: "Product fetched successfully",
+        data: product
+    });
+};
+
+const updateProduct = async (req, res) => {
+    const { id } = req.params;
+    const userId = req.user.id;
+    const productId = parseInt(id, 10);
+    const allowedFields = ["name", "price", "description"];
+    const updateData = {};
+
+    for(const key in req.body) {
+        if(allowedFields.includes(key)) {
+            updateData[key] = req.body[key]
+        }
+    }
+
+    console.log("updateData:", updateData);
+
+    if(Object.keys(updateData).length === 0) {
+        throw new AppError(400, "No valid fields to update");
+    }
+
+    const updatedProduct = await updateProductById(productId, userId, updateData);
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        message: "Successfully updated the product",
+        data: updatedProduct
+    });
+};
+
+const deleteProduct = async () => {
+
+};
+
+export {
+    createProducts,
+    getProducts,
+    getProductsById,
+    updateProduct,
+    deleteProduct
+};
