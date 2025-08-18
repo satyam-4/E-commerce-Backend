ce5487df5fc06dbeafe1adbac02e0bf4693665de
Satyam <sm2003satyam@gmail.com>
2025-08-18 20:25:20 +0530

    Implement becomeSeller and getMe functions for user role management

---
+++ src/modules/user/user.controller.js
@@ -0,0 +1,35 @@
+import { AppError } from "#utils/AppError.js";
+import { createNewSeller } from "./user.db.js";
+
+const becomeSeller = async (req, res) => {
+    const userId = req.user.id;
+    const { pickupAddress, businessName, gstNumber, bankInfo } = req.body;
+
+    if(req.user.role === "seller") {
+        throw new AppError(400, "You are already a seller");
+    }
+
+    const seller = await createNewSeller(userId, pickupAddress, businessName, gstNumber, bankInfo);
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        message: "You are a seller now",
+        data: seller
+    });
+};
+
+const getMe = async (req, res) => {
+    const user = req.user;
+    return res
+    .status(200)
+    .json({
+        user
+    });
+};
+
+export {
+    becomeSeller,
+    getMe
+};
