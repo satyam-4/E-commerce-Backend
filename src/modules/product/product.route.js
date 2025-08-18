c103b80063b6bae26f8d3e767f8e208dd3bfbb10
Satyam <sm2003satyam@gmail.com>
2025-08-18 20:26:02 +0530

    Add product routes for CRUD operations with JWT verification and role checks

---
+++ src/modules/product/product.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { createProducts, getProducts, getProductsById, updateProduct } from "./product.controller.js";
+import { verifyJWT } from "#middlewares/auth.middleware.js";
+import { checkRole } from "#middlewares/role.middleware.js";
+
+const router = express.Router();
+
+router.route("/products").post(verifyJWT, checkRole(["seller"]), createProducts);
+router.route("/products").get(verifyJWT, getProducts);
+router.route("/products/:id").get(verifyJWT, getProductsById);
+router.route("/products/:id").patch(verifyJWT, checkRole(["seller"]), updateProduct);
+
+export default router;
