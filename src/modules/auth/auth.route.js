f63841c7a8ccb88cfcd0254209f25a47d30157c7
Satyam <sm2003satyam@gmail.com>
2025-08-18 20:23:53 +0530

    Remove unused route for retrieving user profile and related middleware from auth routes

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { getMe, signinUser, signupUser } from "./auth.controller.js";
-import { verifyJWT } from "#middlewares/auth.middleware.js";
+import { signinUser, signupUser } from "./auth.controller.js";
 
 const router = express.Router();
 
 router.route("/signin").post(signinUser);
 router.route("/signup").post(signupUser);
-router.route("/me").get(verifyJWT, getMe);
 
 export default router;
