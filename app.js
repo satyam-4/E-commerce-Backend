90758c2fc1893c2a08c348c9f451220d1eaa5782
Satyam <sm2003satyam@gmail.com>
2025-08-18 20:29:18 +0530

    add required routes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import authRoutes from "./src/modules/auth/auth.route.js";
+import userRoutes from "./src/modules/user/user.route.js";
+import productRoutes from "./src/modules/product/product.route.js";
 import { errorHandler } from "./src/middlewares/errorHandler.middleware.js";
 import cookieParser from "cookie-parser";;
 
@@ -9,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/api", productRoutes);
 
 app.use(errorHandler);
 export { app };
