55c55c5a621a3da9a3dce75203995a293d71ba94
Satyam <sm2003satyam@gmail.com>
2025-08-19 21:36:58 +0530

    Add database query to delete product

--- src/modules/product/product.db.js
+++ src/modules/product/product.db.js
@@ -82,3 +82,42 @@ export const updateProductById = async (productId, userId, updateData) => {
         throw new AppError(500, "Error while updating the product");
     }
 };
+
+export const deleteProductById = async (productId, userId) => {
+    try {
+        const seller = await prisma.seller.findFirst({
+            where: {
+                sellerId: userId
+            }
+        });
+
+        const sellerId = seller.id;
+
+        const product = await prisma.product.findFirst({
+            where: {
+                id: productId,
+                sellerId: sellerId
+            }
+        });
+
+        if(!product) {
+            throw new AppError(400, "You cannot delete this product");
+        }
+
+        const deletedProduct = await prisma.product.delete({
+            where: {
+                id: productId
+            }
+        });
+
+        return deletedProduct;
+    } catch (error) {
+        console.log("This is the error->", error)
+
+        if(error instanceof AppError) {
+            throw error;
+        }
+
+        throw new AppError(500, "Error while deleting the product");
+    }
+};
