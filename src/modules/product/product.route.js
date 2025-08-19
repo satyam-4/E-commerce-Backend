b8bdb88bb60bbfc0be869ba71d522f0b4d673e6f
Satyam <sm2003satyam@gmail.com>
2025-08-19 21:36:17 +0530

    Add route for delete product

--- src/modules/product/product.route.js
+++ src/modules/product/product.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProducts, getProducts, getProductsById, updateProduct } from "./product.controller.js";
+import { createProducts, deleteProduct, getProducts, getProductsById, updateProduct } from "./product.controller.js";
 import { verifyJWT } from "#middlewares/auth.middleware.js";
 import { checkRole } from "#middlewares/role.middleware.js";
 
@@ -9,5 +9,6 @@ router.route("/products").post(verifyJWT, checkRole(["seller"]), createProducts)
 router.route("/products").get(verifyJWT, getProducts);
 router.route("/products/:id").get(verifyJWT, getProductsById);
 router.route("/products/:id").patch(verifyJWT, checkRole(["seller"]), updateProduct);
+router.route("/products/:id").delete(verifyJWT, checkRole(["seller"]), deleteProduct);
 
 export default router;
