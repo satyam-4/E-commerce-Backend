50287afb8fbb7df32e17cde4c30bbfe2b09e3988
Satyam <sm2003satyam@gmail.com>
2025-08-22 21:14:21 +0530

    feat(auth): apply signup/signin validators and validate middleware in routes

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { signinUser, signupUser } from "./auth.controller.js";
+import { sigininValidator, signupValidator } from "./auth.validator.js";
+import { validate } from "#middlewares/validate.middleware.js";
 
 const router = express.Router();
 
-router.route("/signin").post(signinUser);
-router.route("/signup").post(signupUser);
+router.route("/signin").post(sigininValidator, validate, signinUser);
+router.route("/signup").post(signupValidator, validate, signupUser);
 
 export default router;
