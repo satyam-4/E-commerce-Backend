220fa859239462a0326e70e5febc454e6a020d33
Satyam <sm2003satyam@gmail.com>
2025-08-24 21:35:06 +0530

    feat(app): add cart routes to the main application

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./src/modules/auth/auth.route.js";
 import userRoutes from "./src/modules/user/user.route.js";
 import productRoutes from "./src/modules/product/product.route.js";
+import cartRoutes from "./src/modules/cart/cart.route.js";
 import { errorHandler } from "./src/middlewares/errorHandler.middleware.js";
 import cookieParser from "cookie-parser";;
 
@@ -10,9 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/api", productRoutes);
+app.use("/cart", cartRoutes);
 
 app.use(errorHandler);
 export { app };
