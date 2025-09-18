f139594ea83b935ee2dd0fdead41620f3f10d82d
Satyam <sm2003satyam@gmail.com>
2025-09-18 12:41:49 +0530

    feat(routes): add seller routes to the API

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import productRoutes from "./src/modules/product/product.route.js";
 import cartRoutes from "./src/modules/cart/cart.route.js";
 import categoryRoutes from "./src/modules/categories/category.route.js";
 import subcategoryRoutes from "./src/modules/subcategories/subcategories.route.js";
+import sellerRoutes from "./src/modules/sellers/seller.route.js";
 import { errorHandler } from "./src/middlewares/errorHandler.middleware.js";
 import cookieParser from "cookie-parser";;
 
@@ -20,6 +21,7 @@ app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/carts", cartRoutes);
 app.use("/api/v1/categories", categoryRoutes);
 app.use("/api/v1/subcategories", subcategoryRoutes);
+app.use("/api/v1/sellers", sellerRoutes);
 
 app.use(errorHandler);
 export { app };
