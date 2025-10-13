a87606e9f184ff52000f937cded36b4c1ca59582
Satyam <sm2003satyam@gmail.com>
2025-10-13 15:37:31 +0530

    fix(middlewares): improve error handling middleware for better logging and response structure

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -1,13 +1,24 @@
 export const errorHandler = (err, req, res, next) => {
-    console.log("Error hander middleware")
-    const statusCode = err.statusCode || 500;
+    console.log("Error handler middleware");
+    const statusCode = err?.statusCode || 500;
 
-    return res
-    .status(statusCode)
-    .json({
-        success: err.success,
-        message: err.message,   
-        errors: err.errors,
-        data: err.data
-    });
+    console.log("code:", statusCode);
+    console.log("error obj:", err);
+
+    if (process.env.NODE_ENV === "development") {
+        return res
+        .status(statusCode)
+        .json({
+            success: false,
+            message: err.message,
+            stack: err.stack
+        })
+    } else {
+        return res
+        .status(statusCode)
+        .json({
+            success: false,
+            message: err.message
+        });
+    }
 };
